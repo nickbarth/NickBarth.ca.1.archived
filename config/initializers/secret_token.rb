4b7224f7d28827fbc4bba77a2caecdc712c6b1f9
Nick Barth <nickbarth@live.com>
2011-10-08 15:50:26 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NickbarthCa::Application.config.secret_token = '5ccc431ffdda573920b8acf31de5f3485d3fa54ef8bc066fa5b6cba1c0175c489772e8aecbe4ed37c7e78313b9d4c5016871d4587c55df9eb4d13571f8dc1082'
